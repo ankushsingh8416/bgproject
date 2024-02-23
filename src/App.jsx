8d9f8f1a06e2a186e6c35e2dda868ee02131dcb1
ankush <nkshrazz@gmail.com>
2024-02-23 00:01:17 -0800

    bg changer

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./index.css";
+import React from "react";
+import First from "./Component/First/First";
+function App() {
+  return (
+    <>
+      <First />
+    </>
+  );
+}
+
+export default App;
